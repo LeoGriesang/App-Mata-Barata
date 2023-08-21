4b3d541f51a8e244a18f18801853a01899558d0f
Leonardo Griesang <griesang.leonardo@outlook.com>
2023-08-21 09:55:15 -0300

    tam e rotacao random

--- script.js
+++ script.js
@@ -49,14 +49,19 @@ function criaBarata() {
         }
     }
 
+    let posicaoX = Math.floor(Math.random() * largura) - 90;
+    let posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
     const barata = document.createElement('img');
     barata.style.position = "absolute"
     barata.src = "imagens/barata1.png"
-    barata.style.height = '70px'
-    barata.style.width = '70px'
-    barata.style.rotate = '30deg'
-    barata.style.top = Math.floor(Math.random() * (altura - 10 + 1)) + 10 + 'px';
-    barata.style.right = Math.floor(Math.random() * (largura - 10 + 1)) + 10 + 'px';
+    barata.classList.add(barataTamanho())
+    barata.classList.add(barataRotacao())
+    barata.style.top = posicaoY + 'px';
+    barata.style.left = posicaoX + 'px';
     barata.id = 'barata'
     document.body.appendChild(barata) 
     console.log(barata)
@@ -65,5 +70,27 @@ function criaBarata() {
     barata.addEventListener('click', clicou)
     function clicou(e) {
         barata.remove()
+    }
+}
+
+function barataRotacao() {
+    rotacao = Math.floor(Math.random() * 2)
+    switch(rotacao) {
+        case 0:
+            return 'rotacaoA'
+        case 1:
+            return 'rotacaoB'
+    }
+}
+
+function barataTamanho() {
+    tamanho = Math.floor(Math.random() * 3)
+    switch(tamanho) {
+        case 0:
+            return 'tamanho1'
+        case 1:
+            return 'tamanho2'
+        case 2:
+            return 'tamanho3'
     }
 }
