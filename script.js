10150afa61fb88d84de4aa908bc74df3e2b6d3d0
Leonardo Griesang <griesang.leonardo@outlook.com>
2023-08-21 11:11:56 -0300

    ajuste

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function criaBarata() {
 
     const barata = document.createElement('img');
     barata.style.position = "absolute"
-    barata.src = "imagens/barata1.svg"
+    barata.src = "imagens/Barata1.png"
     barata.classList.add(barataTamanho())
     barata.classList.add(barataRotacao())
     barata.style.top = posicaoY + 'px';
